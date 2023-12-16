49ad4b00d28e74f1d19ff33e9f245dcfb4edc17d
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2023-12-16 17:45:18 +0530

    Refactor code to improve performance and readability

---
+++ OPENCV/reading_videos.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+# vid = cv.VideoCapture(0) # uses webcam
+vid = cv.VideoCapture('OPENCV/video/dog.mp4') # uses video file
+
+while True:
+    isTrue, frame = vid.read()
+    cv.imshow('Video', frame)
+    
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+    
+
+vid.release()
+cv.destroyAllWindows()
