192ba626938195965e99b41b10077f22f15b19b3
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2023-12-17 15:06:54 +0530

    Update rescaleFrame function to use a smaller scale

--- OPENCV/tutorials/rescale.py
+++ OPENCV/tutorials/rescale.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-def rescaleFrame(frame, scale=0.75):
+def rescaleFrame(frame, scale=0.2):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
@@ -11,7 +11,7 @@ def rescaleFrame(frame, scale=0.75):
 
 img = cv.imread('OPENCV/images/dog.jpg')
 
-resiged_image = rescaleFrame(img, scale=0.2)
+resiged_image = rescaleFrame(img, scale=0.1)
 
 cv.imshow("Dog", resiged_image)
 cv.imshow("DOG", img)
@@ -19,17 +19,17 @@ cv.imshow("DOG", img)
 
 
 # Rescaling of Video  
-# vid = cv.VideoCapture('OPENCV/video/dog.mp4') # uses video file
+vid = cv.VideoCapture('OPENCV/video/dog.mp4') # uses video file
 
-# while True:
-#     isTrue, frame = vid.read(0)
+while True:
+    isTrue, frame = vid.read(0)
     
-#     frame_resized = rescaleFrame(frame, scale=0.2)
+    frame_resized = rescaleFrame(frame, scale=0.2)
     
-#     cv.imshow('Video', frame)
-#     cv.imshow("Video Resized", frame_resized)
-#     if cv.waitKey(20) & 0xFF==ord('d'):
-#         break
+    cv.imshow('Video', frame)
+    cv.imshow("Video Resized", frame_resized)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
     
 
 cv.waitKey(0)
